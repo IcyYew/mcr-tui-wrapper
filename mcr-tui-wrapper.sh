93ee7cb8f1c50203609529555c7852600ab51ac1
IcyYew <icy.yew02@gmail.com>
2026-08-09 18:57:51 -0500

    Script file and initial script variables

---
+++ mcr-tui-wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Configure these based upon your RCON settings
+RCON_HOST=""
+RCON_PORT=""
+# Set this as an env var rather than setting directly
+RCON_PASSWORD=""
+
+# Set path to mcrcon exe
+MCRCON_PATH=""
+
+# Easy typing lul
+MCRCON="$MCRCON_PATH"
+
+
