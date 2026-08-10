ec7f925775c0dfd8b0ddb62e636456ef8b2ab12b
IcyYew <icy.yew02@gmail.com>
2026-08-09 19:58:35 -0500

    Fixing init

--- init.sh
+++ init.sh
@@ -4,15 +4,15 @@
 
 
 printf "Input RCON Hostname (ex. 127.0.0.1 || yourhost.com): "
-read $RCON_HOST
+read RCON_HOST
 
 printf "Input RCON Port (ex. 25575): "
-read $RCON_PORT
+read RCON_PORT
 
 printf "Input RCON Password (ex. password): "
-read $RCON_PASSWORD
+read RCON_PASSWORD
 
 printf "Input mcrcon Path (ex. /path/to/mcrcon.sh)"
-read $MCRCON_PATH
+read MCRCON_PATH
 
 declare -p RCON_HOST RCON_PORT RCON_PASSWORD MCRCON_PATH
