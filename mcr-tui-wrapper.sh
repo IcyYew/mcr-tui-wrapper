7b0443829639e4cbff2c9a807935cdcb1211ba94
IcyYew <icy.yew02@gmail.com>
2026-08-09 20:38:27 -0500

    users command

--- mcr-tui-wrapper.sh
+++ mcr-tui-wrapper.sh
@@ -13,8 +13,24 @@ MCRCON_PATH="${MCRCON_PATH}"
 
 MCRCON="${MCRCON_PATH}"
 
-
+COMMAND=$1
 
 list() {
 	"$MCRCON" -H "$RCON_HOST" -p "$RCON_PASSWORD" list
 }
+
+if [ -z "$COMMAND" ]; then
+	echo "ERR: NO CMD"
+	exit 1
+fi
+
+case "$COMMAND" in
+	"users")
+		list
+		;;
+	*)
+		echo "Fallback"
+		exit 1
+		;;
+esac
+
