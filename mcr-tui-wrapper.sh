77f54ead82e359a980c925c9dcc2388d82dbde9f
IcyYew <icy.yew02@gmail.com>
2026-08-10 14:50:19 -0500

    Comments added to TUI-wrapper and function library

--- mcr-tui-wrapper.sh
+++ mcr-tui-wrapper.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+#
+#
+#  IMPORTS
+#
+#
 . ./.env
 . ./mcrw-functions.lib
+
 #
 #
 # 	ENV VARS
 #
 #
 
+# These can be removed at some point since all rcon communications are handled in function libaries
 # Variable definitions for visibility, all are actually sourced from .env, if you do not have a .env run init.sh and input your values
 RCON_HOST="${RCON_HOST}"
 RCON_PORT="${RCON_PORT}"
@@ -15,22 +22,33 @@ RCON_PASSWORD="${RCON_PASSWORD}"
 MCRCON_PATH="${MCRCON_PATH}"
 MCRCON="${MCRCON_PATH}"
 
+# Likely useless abstraction, once the TUI becomes multiple "pages" these may improve readability
+MENU_LAYER_1_OPTIONS=""
+MENU_LAYER_1=""
+
+# Main TUI loop
 while :
 do
+	# Clean up screen
 	clear
 	printf "MCR TUI WRAPPER\n1. LIST USERS\n2. SEND MESSAGE TO ALL USERS\n9. EXIT\n"
 	read SELECTION_VAR
 	case "$SELECTION_VAR" in
+		# Case 1 lists all active users and quantity of users
 		1) clear
+			# Define OUTPUT variable to read the output of the list function
 			OUTPUT=$(list 2>&1)
 			printf '%s\n' "$OUTPUT"
 			wait_for_user
 		 ;;
+		 # Case 2 sends a message to all users on the server as the server
 		2) clear
 			printf "Input text to send to all users: "
 			read TEXT
 			OUTPUT=$(tellall 2>&1)
 			clear
+			# Simple check that doesn't do much of anything, can be expanded upon later (conditions in which "Message not sent" prints are
+			# currently zero
 			if [ "$OUTPUT" = 0 ]; then
 				printf "\nMessage not sent"
 				wait_for_user
@@ -40,6 +58,7 @@ do
 			fi
 
 		 ;;
+		 # Case 9 obvious
 	 	9) clear
 			printf "Bye bye! o/\n"
 			wait_for_user
