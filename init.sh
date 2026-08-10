28e16347b856fc5ba178a2aa8a9c3fbe2fd73736
IcyYew <icy.yew02@gmail.com>
2026-08-09 19:54:53 -0500

    Added init script

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# init env vars for mcrcon tui wrapper
+
+
+printf "Input RCON Hostname (ex. 127.0.0.1 || yourhost.com): "
+read $RCON_HOST
+
+printf "Input RCON Port (ex. 25575): "
+read $RCON_PORT
+
+printf "Input RCON Password (ex. password): "
+read $RCON_PASSWORD
+
+printf "Input mcrcon Path (ex. /path/to/mcrcon.sh)"
+read $MCRCON_PATH
+
+declare -p RCON_HOST RCON_PORT RCON_PASSWORD MCRCON_PATH
--- mcr-tui-wrapper.sh
+++ mcr-tui-wrapper.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-# Configure these based upon your RCON settings
-RCON_HOST=""
-RCON_PORT=""
+# Can be configured automatically via init or manually if you wish
+RCON_HOST="${RCON_HOST}"
+RCON_PORT="${RCON_PORT}"
 # Set this as an env var rather than setting directly
-RCON_PASSWORD=""
+RCON_PASSWORD="${RCON_PASSWORD}"
 
 # Set path to mcrcon exe, if env then mcrcon (or whatever it's command is
-MCRCON_PATH=""
+MCRCON_PATH="${MCRCON_PATH}"
 
-# Easy typing lul
-MCRCON="$MCRCON_PATH"
+# Automatic exporting ON
+set -a
+
+# Take in init.sh output
+eval "$(./init.sh)"
+
+# Automatic exporting OFF
+set +a
+
+MCRCON="${MCRCON_PATH}"
 
 $MCRCON -H $RCON_HOST -p $RCON_PASSWORD list
