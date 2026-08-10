38d35e6b01203cb7f9a2089d9bb2a407b61c94b7
IcyYew <icy.yew02@gmail.com>
2026-08-09 19:38:11 -0500

    Testing list

--- mcr-tui-wrapper.sh
+++ mcr-tui-wrapper.sh
@@ -6,10 +6,10 @@ RCON_PORT=""
 # Set this as an env var rather than setting directly
 RCON_PASSWORD=""
 
-# Set path to mcrcon exe
+# Set path to mcrcon exe, if env then mcrcon (or whatever it's command is
 MCRCON_PATH=""
 
 # Easy typing lul
 MCRCON="$MCRCON_PATH"
 
-
+$MCRCON -H $RCON_HOST -p $RCON_PASSWORD list
