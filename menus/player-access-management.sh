02e720185406afc2259ee29f4d709663dd10b965
IcyYew <icy.yew02@gmail.com>
2026-08-11 15:45:51 -0500

    Player access management menu done outside of whitelist

--- menus/player-access-management.sh
+++ menus/player-access-management.sh
@@ -11,7 +11,7 @@ player_access_management_menu() {
 while :
 do
 	clear
-	printf "PLAYER ACCESS MANAGEMENT\n1. BAN\n2. UNBAN\n3. BANLIST\n4. UNBAN\n5. WHITELIST\n9. BACK\n"
+	printf "PLAYER ACCESS MANAGEMENT\n1. BAN\n2. UNBAN\n3. BANLIST\n4. BAN IP\n5. UNBAN IP\n6. WHITELIST\n9. BACK\n"
 	read PA_MGM_VAR
 	case $PA_MGM_VAR in
 		1)	
@@ -31,14 +31,25 @@ do
 			wait_for_user
 		;;
 		3)
+			clear
+			list_banlist
 
 		;;
 		4)
+			clear
+			printf "Input IP address: "
+			read IP_ADDR
+			ban_ip
 
 		;;
 		5)
+			clear
+			print "Input IP address: "
+			read IP_ADDR
+			unban_ip
 
 		;;
+		6) whitelist_management_menu ;;
 		9) break ;;
 		*) printf "Invalid option" ;;
 	esac
