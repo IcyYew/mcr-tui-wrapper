1b4f59a5ac380a2fa3c5f86a320c2b40396f88c1
IcyYew <icy.yew02@gmail.com>
2026-08-11 12:49:34 -0500

    Leaving test branch, largely irrelevant due to current implementation being deterministic

--- test/advancement-test.sh
+++ test/advancement-test.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-. ./libs/test.lib
+. ./libs/advancements.lib
+
+rcon() {
+	printf '%s\n' "$1"
+}
 
 
 PASS=0
 FAIL=0
 
 # test grant every advancement with player target identifier
-[ $(neutered_advancements_function "grant" "@a" "everything") = "advancement grant @a everything" ] && PASS=PASS+1 || FAIL=FAIL+1 
+OUTPUT=$(advancements_function "grant" "@a" "everything")
+[ "$OUTPUT" = "advancement grant @a everything" ] && PASS=$((PASS + 1)) || FAIL=$((FAIL + 1))
+
+# test grant only advancement
+OUTPUT=$(advancements_function "grant" "@a" "only" "minecraft:story/shiny_gear")
+[ "$OUTPUT" = "advancement grant @a everything minecraft:story/shiny_gear" ] && PASS=$((PASS + 1)) || FAIL=$((FAIL + 1))
+
+# test grant from advancement
+
+printf "PASS: $PASS\nFAIL: $FAIL\n"
